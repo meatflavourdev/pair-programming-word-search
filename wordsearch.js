aef3a6b5e0da6ebc47a17c5f3492f481267abbde
Jeremy Felix D <me@jeremyfelix.com>
2020-11-04 18:07:25 -0800

    Pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const joinVertical = function (letters) {
+    const result = [];
+      if(!letters.length) return [];
+      if(!letters[0].length) return [];
+  
+    for (let i = 0; i < letters[0].length; i++) {
+      let verticalString = "";
+      for (let j = 0; j < letters.length; j++) {
+        verticalString += letters[j][i];
+      }
+      result.push(verticalString);
     }
-}
-
-module.exports = wordSearch
+    return result;
+  };
+  
+  
+  const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = joinVertical(letters);
+  
+  
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
